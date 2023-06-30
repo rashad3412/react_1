3a7dc6a488fce85bb6364e26f6ed1974d9cdedcd
rashad3412 <rashd3412@gmail.com>
2023-06-29 20:49:54 -0400

    updated react

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,42 @@ import './App.css'
 function App() {
 
 const [puppies, setPuppies] = useState(puppyList);
-  console.log("puppyList: ", puppyList);
+const [featPupId, setFeatPupId] = useState(null);
+  function handleClick(puppyId){
+    setFeatPupId(puppyId)
+
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
   return (
     <div className='App'>
 
       {
-        puppies.map((puppy) => {
-          return <p key = {puppy.id} > {puppy.name}</p>;
+        puppies.map((puppy) => { 
+          return (
+          
+          <p onClick ={()=>{handleClick(puppy.id)}} key = {puppy.id} > {puppy.name}</p>
+          
+          )
+
         })
       }
+
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}  
       
+
+    
+
     </div>
-  )
-};
+  );
+}
 
 export default App
